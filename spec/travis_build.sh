1c6bb7ed8482df1b369f590346fb322a7d10b79e
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-12-18 13:10:13 +0200

    Add LD_FLAGS and C_FLAGS environment variables

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,5 +1,4 @@
 #/usr/bin/env sh
 git clone https://github.com/Hello71/zopfli.git
 cd zopfli
-make && sudo make install
-sudo ln -fs /usr/local/lib/libzopfli.* /usr/lib/
+make && make prefix=$HOME/opt install
