9d9ac663a8bf47ba1034970f39362e653b58a619
Alexey Vasiliev <leopard.not.a@gmail.com>
2015-06-16 15:12:40 +0300

    fix build

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,4 +1,5 @@
 #/usr/bin/env sh
 git clone https://github.com/Hello71/zopfli.git
 cd zopfli
-make -j2 && make -j2 test && make --prefix=$HOME/opt -j2 install
+git submodule update --init
+make -j2 && make prefix=$HOME/opt -j2 install
