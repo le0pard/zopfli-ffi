d640accfd3f877ae00037c1e5e70d721c23d6916
le0pard <leopard.not.a@gmail.com>
2015-06-16 12:46:45 +0300

    new lib

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,4 +1,4 @@
 #/usr/bin/env sh
-git clone https://github.com/Hello71/zopfli.git
+git clone https://github.com/google/zopfli.git
 cd zopfli
-make -j2 && make -j2 test && make prefix=$HOME/opt install
+make && make libzopfli
