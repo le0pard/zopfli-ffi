26900aad4db2b0c1ea8734ac796e02a8babcf930
le0pard <leopard.not.a@gmail.com>
2015-06-16 13:05:45 +0300

    new lib

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -3,24 +3,24 @@ git clone https://github.com/google/zopfli.git
 cd zopfli
 make && make libzopfli && make zopflipng && make libzopflipng
 
-prefix = $HOME/opt
-exec_prefix = $(prefix)
-bindir = $(exec_prefix)/bin
-libdir = $(exec_prefix)/lib
-includedir = $(prefix)/include
+prefix := $HOME/opt
+exec_prefix := $(prefix)
+bindir := $(exec_prefix)/bin
+libdir := $(exec_prefix)/lib
+includedir := $(prefix)/include
 
-mkdir -p $(DESTDIR)$(bindir)
-install -m755 zopfli zopflipng $(DESTDIR)$(bindir)
-mkdir -p $(DESTDIR)$(libdir)
-install -m755 libzopfli.so.1.0.1 $(DESTDIR)$(libdir)
+mkdir -p $(bindir)
+install -m755 zopfli zopflipng $(bindir)
+mkdir -p $(libdir)
+install -m755 libzopfli.so.1.0.1 $(libdir)
 cp -d libzopfli.so libzopfli.so.1.0.1 libzopfli.so.1 \
       libzopflipng.so libzopflipng.so.1.0.0 libzopflipng.so.1 \
-      $(DESTDIR)$(libdir)
-mkdir -p $(DESTDIR)$(includedir)/zopfli
+      $(libdir)
+mkdir -p $(includedir)/zopfli
 install -m644 src/zopfli/deflate.h src/zopfli/zlib_container.h \
               src/zopfli/zopfli.h src/zopfli/katajainen.h \
 	      src/zopfli/tree.h src/zopfli/gzip_container.h \
 	      src/zopfli/cache.h src/zopfli/squeeze.h \
 	      src/zopfli/lz77.h src/zopfli/util.h \
 	      src/zopfli/blocksplitter.h src/zopfli/hash.h \
-	      $(DESTDIR)$(includedir)/zopfli
+	      $(includedir)/zopfli
