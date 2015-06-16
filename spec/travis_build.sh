12e97f0211bd0c64d7c5f55765d123f23d073d01
le0pard <leopard.not.a@gmail.com>
2015-06-16 13:19:21 +0300

    new lib

--- spec/travis_build.sh
+++ spec/travis_build.sh
@@ -1,20 +1,4 @@
 #/usr/bin/env sh
-git clone https://github.com/google/zopfli.git
+git clone https://github.com/Hello71/zopfli.git
 cd zopfli
-make && make libzopfli && make zopflipng && make libzopflipng
-
-mkdir -p $HOME/opt/bin
-install -m755 zopfli zopflipng $HOME/opt/bin
-mkdir -p $HOME/opt/lib
-install -m755 libzopfli.so.1.0.1 $HOME/opt/lib
-cp -d libzopfli.so libzopfli.so.1.0.1 \
-      libzopflipng.so libzopflipng.so.1.0.0 \
-      $HOME/opt/lib
-mkdir -p $HOME/opt/include/zopfli
-install -m644 src/zopfli/deflate.h src/zopfli/zlib_container.h \
-              src/zopfli/zopfli.h src/zopfli/katajainen.h \
-	      src/zopfli/tree.h src/zopfli/gzip_container.h \
-	      src/zopfli/cache.h src/zopfli/squeeze.h \
-	      src/zopfli/lz77.h src/zopfli/util.h \
-	      src/zopfli/blocksplitter.h src/zopfli/hash.h \
-	      $HOME/opt/include/zopfli
+make -j2 && make -j2 test && make --prefix=$HOME/opt -j2 install
